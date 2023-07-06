8d28224f0eaf231043e1a8929cbcd130689b0d08
thanap151255 <thanap151255@gmail.com>
2023-07-06 21:49:45 +0700

    2.5

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const {createApp, ref} = Vue
 createApp({
   setup(){
     const product = ref('boots')
+    const description = ref('this is a good boots for traveler!')
     return{
-      product
+      product , description
     }
  }
 
