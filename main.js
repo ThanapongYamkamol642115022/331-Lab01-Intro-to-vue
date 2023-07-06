87f4d8cdfa7f83d5d107da54f459c1dcdbcd762b
thanap151255 <thanap151255@gmail.com>
2023-07-06 22:20:30 +0700

    5.5

--- main.js
+++ main.js
@@ -9,14 +9,18 @@ createApp({
     const link = ref("https://www.camt.cmu.ac.th/");
     const inStock = ref(true)
     const inventory = ref(100)
-    const details = ref([
-      '50% cotton' , '30% wool' , '20% polyester'
-    ])
+    const details = ref(["50% cotton", "30% wool", "20% polyester"]);
     const variants = ref([
-      {id : 2234 , color : 'green'} , {id : 2335 , color: 'blue'}
-    ])
+      { id: 2234, color: "green" },
+      { id: 2235, color: "blue" },
+    ]);
+    const sizes = ref([
+      { id: 1, size: "S" },
+      { id: 2, size: "M" },
+      { id: 3, size: "L" },
+    ]);
     return{
-      product , image , link , inStock , inventory , details , variants
+      product , image , link , inStock , inventory , details , variants , sizes
     }
  }
 
