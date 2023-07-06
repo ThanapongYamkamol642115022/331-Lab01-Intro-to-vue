26b0d4047e8ec3996b14974ad35edaac39d685b8
thanap151255 <thanap151255@gmail.com>
2023-07-06 21:57:54 +0700

    3.6

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ createApp({
     const product = ref('boots')
     const description = ref('this is a good boots for traveler!')
     const image = ref('./assets/images/socks_green.jpg')
+    const link = ref("https://www.camt.cmu.ac.th/");
     return{
-      product , description , image
+      product , description , image , link
     }
  }
 
