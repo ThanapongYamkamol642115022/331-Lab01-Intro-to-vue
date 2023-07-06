8eff4d167661dacac2d4c2138980c9a41d2ac328
thanap151255 <thanap151255@gmail.com>
2023-07-06 22:03:22 +0700

    4.5

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ createApp({
     const image = ref('./assets/images/socks_green.jpg')
     const link = ref("https://www.camt.cmu.ac.th/");
     const inStock = ref(true)
+    const inventory = ref(100)
     return{
-      product , description , image , link , inStock
+      product , description , image , link , inStock , inventory
     }
  }
 
