168717a00b45a599e856a5b47bbadac6b557fb81
thanap151255 <thanap151255@gmail.com>
2023-07-16 16:51:12 +0700

    8.3

--- main.js
+++ main.js
@@ -6,15 +6,16 @@ createApp({
     const product = ref('boots')
     const brand = ref('SE 331')
     // const description = ref('this is a good boots for traveler!')
-    const image = ref('./assets/images/socks_green.jpg')
+    // const image = ref('./assets/images/socks_green.jpg')
     const link = ref("https://www.camt.cmu.ac.th/");
-    const inStock = ref(true)
+    // const inStock = ref(true)
     const inventory = ref(100)
     const details = ref(["50% cotton", "30% wool", "20% polyester"]);
     const variants = ref([
-      { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-      { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" }
+      { id: 2234, color: "green", image: "./assets/images/socks_green.jpg", quantity: 50 },
+      { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg", quantity: 0 }
     ]);
+    const selectVariant = ref(0)
     const sizes = ref([
       { id: 1, size: "S" },
       { id: 2, size: "M" },
@@ -35,6 +36,18 @@ createApp({
     function changeInStock(){
       inStock.value = !inStock.value;
     }
+
+    function updateVariant(index){
+      selectVariant.value = index;
+    }
+
+    const image = computed(() =>{
+      return variants.value[selectVariant.value].image
+    })
+
+    const inStock = computed(() =>{
+      return variants.value[selectVariant.value].quantity
+    })
 
     return{
       // product , 
