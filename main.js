878becad6ac05a1d943c6759585e8db14463e1e6
thanap151255 <thanap151255@gmail.com>
2023-08-02 16:43:26 +0700

    9.6

--- main.js
+++ main.js
@@ -1,83 +1,18 @@
 // const product = 'Socks'
 const {createApp, ref, computed } = Vue
 
-createApp({
+const app = createApp({
   setup(){
-    const product = ref('boots')
-    const brand = ref('SE 331')
-    const onSale = ref(false)
-    // const description = ref('this is a good boots for traveler!')
-    // const image = ref('./assets/images/socks_green.jpg')
-    const link = ref("https://www.camt.cmu.ac.th/");
-    // const inStock = ref(true)
-    const inventory = ref(100)
-    const details = ref(["50% cotton", "30% wool", "20% polyester"]);
-    const variants = ref([
-      { id: 2234, color: "green", image: "./assets/images/socks_green.jpg", quantity: 50 },
-      { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg", quantity: 0 }
-    ]);
-    const selectVariant = ref(0)
-    const sizes = ref([
-      { id: 1, size: "S" },
-      { id: 2, size: "M" },
-      { id: 3, size: "L" },
-    ]);
+    
+   
     const cart = ref(0)
-    function addToCart(){
-      cart.value +=1
-    }
-
-    const title = computed(() =>{
-      if(!onSale.value){
-        console.log(0);
-
-        return brand.value + ' ' + product.value;
-      }
-      if (onSale.value) {
-        console.log(1);
-        console.log(onSale);
-
-        return brand.value + ' ' + product.value + " is on sale"
-      }
-    })
     
-    function updateImage(variantImage){
-      image.value = variantImage
-    }
-    
-    function changeInStock(){
-      inStock.value = !inStock.value;
-    }
-
-    const updateVariant = (index) =>{
-      selectVariant.value = index;
-    }
-
-    const image = computed(() =>{
-      return variants.value[selectVariant.value].image
-    })
-
-    const inStock = computed(() =>{
-      return variants.value[selectVariant.value].quantity
-    })
 
     return{
-      // product , 
-      image , 
-      link , 
-      inStock , 
-      inventory , 
-      details , 
-      variants , 
-      sizes , 
-      cart , 
-      addToCart , 
-      updateImage , 
-      changeInStock ,
-      title,
-      updateVariant
-      // brand
-    }
- }
+      cart ,     
+    };
+ },
+});
 
-}).mount('#app')
+app.component("product-display", productDisplay);
+app.mount("#app");
